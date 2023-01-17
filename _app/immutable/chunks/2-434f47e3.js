b2a60d7f02abfe765148eb25ba60c808574148c2
Corinna Steinhoff <github@corinnasteinhoff.de>
2023-01-17 12:56:16 +0100

    Updates

---
+++ _app/immutable/chunks/2-434f47e3.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-4f7d8d20.js";export{t as component};
--- _app/immutable/chunks/2-72ac85c3.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-0ae1b83b.js";export{t as component};
